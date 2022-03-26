c56ab860f4317e4d0b6fd571ddf2949ce2e74166
Shahed Rahim <shahed21@gmail.com>
2022-03-26 11:58:24 -0500

    Init

---
+++ lib/adder/adder.h
@@ -0,0 +1,13 @@
+#ifndef ADDER_H
+#define ADDER_H
+
+/**
+ * @brief fundtion declaration for add function that is to be tested
+ * 
+ * @param a 
+ * @param b 
+ * @return int 
+ */
+int add(int a, int b);
+
+#endif //ADDER_H
---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <adder.h>
+
+int main(int argc, char* argv) {
+    int a=10;
+    int b=20;
+    printf("a+b=%d\n", add(a,b));
+}
