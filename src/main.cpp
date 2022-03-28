9457de42550c78966e4410812e8999dd3bd41429
Shahed Rahim <shahed21@gmail.com>
2022-03-28 11:54:41 -0500

    Better Tests

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,22 @@
 #include <adder.h>
 #include <gtest/gtest.h>
 
-TEST(TestSuiteSample, TestSample)
+TEST(AdderTests, AdderNETest)
 {
-    ASSERT_EQ(6, 1+5);
-}
-
-int main(int argc, char** argv) {
     int a=10;
     int b=20;
-    printf("a+b=%d\n", add(a,b));
+    ASSERT_NE(6, add(a,b));
+}
 
+TEST(AdderTests, AdderEQTest)
+{
+    int a=10;
+    int b=20;
+    ASSERT_EQ(30, add(a,b));
+}
+
+
+int main(int argc, char** argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
