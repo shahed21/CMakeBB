6425d8bd7074d0cc908fa6ec50edbde3bf2743df
Shahed Rahim <shahed21@gmail.com>
2022-04-01 08:53:16 -0500

    Version tests working

--- lib/samplelib/samplelib.h
+++ lib/samplelib/samplelib.h
@@ -14,12 +14,48 @@
 #include <config.h>
 
 /**
- * @brief fundtion declaration for add function that is to be tested
+ * @brief fundtion declaration for samplelib_add function that is to be tested
  * 
  * @param a 
  * @param b 
- * @return int 
+ * @return int a+b
  */
-int add(int a, int b);
+int samplelib_add(int a, int b);
+
+/**
+ * @brief function declaration for samplelib_getLibraryName function
+ * 
+ * @return char* return the name of the library compiled in library memory
+ */
+char* samplelib_getLibraryName();
+
+/**
+ * @brief function declaration for samplelib_getLibraryVersion function
+ * 
+ * @return char* return the version number of the library as a string
+ */
+char* samplelib_getLibraryVersion();
+
+/**
+ * @brief function declaration for samplelib_getLibraryVersionMajor function
+ * 
+ * @return char* return the version major number of the library as a string
+ */
+char* samplelib_getLibraryVersionMajor();
+
+/**
+ * @brief function declaration for samplelib_getLibraryVersionMinor function
+ * 
+ * @return char* return the version minor number of the library as a string
+ */
+char* samplelib_getLibraryVersionMinor();
+
+/**
+ * @brief function declaration for samplelib_getLibraryVersionPatch function
+ * 
+ * @return char* return the version patch number of the library as a string
+ */
+char* samplelib_getLibraryVersionPatch();
+
 
 #endif //SAMPLELIB_H
--- src/SampleLibTests.cpp
+++ src/SampleLibTests.cpp
@@ -1,19 +1,46 @@
 //#include <stdio.h>
 #include <samplelib.h>
 #include <gtest/gtest.h>
+#include <config.h>
 
 TEST(SampleLibTests, SampleLibNETest)
 {
     int a=10;
     int b=20;
-    ASSERT_NE(6, add(a,b));
+    ASSERT_NE(6, samplelib_add(a,b));
 }
 
 TEST(SampleLibTests, SampleLibEQTest)
 {
     int a=10;
     int b=20;
-    ASSERT_EQ(30, add(a,b));
+    ASSERT_EQ(30, samplelib_add(a,b));
+}
+
+
+TEST(SampleLibVersionTests, SampleLibNameTest)
+{
+    ASSERT_STREQ(PROJECT_NAME, samplelib_getLibraryName());
+}
+
+TEST(SampleLibVersionTests, SampleLibVerTest)
+{
+    ASSERT_STREQ(PROJECT_VER, samplelib_getLibraryVersion());
+}
+
+TEST(SampleLibVersionTests, SampleLibVerMajorTest)
+{
+    ASSERT_STREQ(PROJECT_VER_MAJOR, samplelib_getLibraryVersionMajor());
+}
+
+TEST(SampleLibVersionTests, SampleLibVerMinorTest)
+{
+    ASSERT_STREQ(PROJECT_VER_MINOR, samplelib_getLibraryVersionMinor());
+}
+
+TEST(SampleLibVersionTests, SampleLibVerPatchTest)
+{
+    ASSERT_STREQ(PROJECT_VER_PATCH, samplelib_getLibraryVersionPatch());
 }
 
 
