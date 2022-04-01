217eeb038e80cf7460828d31d73625903fe4546c
Shahed Rahim <shahed21@gmail.com>
2022-04-01 05:42:25 -0500

    config.h has version information

--- lib/samplelib/samplelib.h
+++ lib/samplelib/samplelib.h
@@ -1,5 +1,17 @@
+/**
+ * @file samplelib.h
+ * @author Shahed Rahim (shahed21@gmail.com)
+ * @brief this is a sample header for the samplelib.cpp file
+ * @version 0.1
+ * @date 2022-03-31
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #ifndef SAMPLELIB_H
 #define SAMPLELIB_H
+
+#include <config.h>
 
 /**
  * @brief fundtion declaration for add function that is to be tested
